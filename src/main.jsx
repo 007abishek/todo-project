36a7efc2cddc113cdf7b5a1368b9a1116c578828
Developer <dev@example.com>
2026-01-19 23:11:07 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import React from "react";
 import App from "./App";
 import { AuthProvider } from "./auth/AuthContext";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
- 
+  <React.StrictMode>
     <AuthProvider>
-       <App />
+      <App />
     </AuthProvider>
-
-  
+  </React.StrictMode>
 );
